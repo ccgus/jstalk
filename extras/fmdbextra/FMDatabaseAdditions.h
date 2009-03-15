d24bd62670383d271f3f909baf3e777fe8315818
August Mueller <gus@flyingmeat.com>
2009-03-15 12:29:18 -0700

    added a new jstalk extra, using fmdb, as well as adding support to load bridgesupport xml files from the jstalk extras

---
+++ extras/fmdbextra/FMDatabaseAdditions.h
@@ -0,0 +1,24 @@
+//
+//  FMDatabaseAdditions.h
+//  fmkit
+//
+//  Created by August Mueller on 10/30/05.
+//  Copyright 2005 Flying Meat Inc.. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@interface FMDatabase (FMDatabaseAdditions)
+
+
+- (int) intForQuery:(NSString*)objs, ...;
+- (long) longForQuery:(NSString*)objs, ...; 
+- (BOOL) boolForQuery:(NSString*)objs, ...;
+- (double) doubleForQuery:(NSString*)objs, ...;
+- (NSString*) stringForQuery:(NSString*)objs, ...; 
+- (NSData*) dataForQuery:(NSString*)objs, ...;
+
+// Notice that there's no dataNoCopyForQuery:.
+// That would be a bad idea, because we close out the result set, and then what
+// happens to the data that we just didn't copy?  Who knows, not I.
+
+@end
